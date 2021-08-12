4b945a99047269604cda3a62e300e6817530706f
datprovip123 <thanhdat8401@gmail.com>
2021-08-12 21:29:52 +0700

    git init

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+//$connect = mysqli_connect('localhost','root','','web_music');
+//if (!$connect){
+//echo "ket noi that bai";
+
+$server = "35.172.43.136";
+$username = "datvip";
+$password = "datvip";
+$dbname ="datvip";
+$connect = mysqli_connect($server, $username, $password, $dbname);
+}
+?>
