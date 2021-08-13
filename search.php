303cd087fdb9c9502185ee640738ff4ba2794c4b
datprovip123 <thanhdat8401@gmail.com>
2021-08-13 12:12:33 +0700

    git init9

--- search.php
+++ search.php
@@ -76,7 +76,7 @@ img.d-block {
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="index.php">Sound Clownz</a>
+                <a class="navbar-brand" href="index.php">Marvel Store</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
--- thanhtoan.php
+++ thanhtoan.php
@@ -78,7 +78,7 @@ img.d-block {
     <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="index.php">Sound Clownz</a>
+                <a class="navbar-brand" href="index.php">Marvel Store</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
