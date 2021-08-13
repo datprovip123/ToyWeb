e1c962a35d377bd0864d7d823e3f5bcc64805d13
datprovip123 <thanhdat8401@gmail.com>
2021-08-13 10:20:14 +0700

    git init 7

--- connect.php
+++ connect.php
@@ -4,4 +4,5 @@ $username = "datprovip";
 $password = "datprovip";
 $dbname ="toywebsite";
 $connect = mysqli_connect($server, $username, $password, $dbname);
+mysqli_set_charset($connect,'UTF8');
 ?>
